57712ead9e982c90eb7f9f92af5689c58bff63b9
Alec Delaney <tekktrik@gmail.com>
2026-03-15 21:23:00 -0400

    Remove simulator script

--- scripts/simulate.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2026 Alec Delaney
-# SPDX-License-Identifier: MIT
-
-"""CircuitPython simulator script."""
-
-import circuitpy_sim
-
-circuitpy_sim.simulate_circuitpython()
